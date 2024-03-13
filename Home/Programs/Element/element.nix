75a6cfddbfbf503b26edeff219550d0d1486ff75
AlephNought <radkohruska@gmail.com>
2024-03-13 06:41:43 +0100

    Revert Vesktop vulkan flag and add it to Element instead

--- Home/Programs/Element/element.nix
+++ Home/Programs/Element/element.nix
@@ -11,7 +11,7 @@
         makeWrapper '${electron}/bin/electron' "$out/bin/element-desktop" \
           --set LD_PRELOAD ${sqlcipher}/lib/libsqlcipher.so \
           --add-flags "$out/share/element/electron" \
-          --add-flags "--ozone-platform=wayland --enable-zero-copy --use-gl=angle --use-vulkan --disable-reading-from-canvas --enable-oop-rasterization --enable-raw-draw --enable-gpu-rasterization --enable-gpu-compositing --enable-native-gpu-memory-buffers --enable-accelerated-2d-canvas --enable-accelerated-video-decode --enable-accelerated-mjpeg-decode --disable-gpu-vsync --enable-features=Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport"
+          --add-flags "--ozone-platform=wayland --enable-zero-copy --use-gl=angle --use-vulkan --use-angle=vulkan --disable-reading-from-canvas --enable-oop-rasterization --enable-raw-draw --enable-gpu-rasterization --enable-gpu-compositing --enable-native-gpu-memory-buffers --enable-accelerated-2d-canvas --enable-accelerated-video-decode --enable-accelerated-mjpeg-decode --disable-gpu-vsync --enable-features=Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport"
       ''; # Vulkan stuff is left here because we want to make --use-angle=vulkan work.
     })
   ];
--- Home/Programs/Vesktop/vesktop.nix
+++ Home/Programs/Vesktop/vesktop.nix
@@ -10,8 +10,8 @@
       patches = (old.patches or []) ++ [./readonlyFix.patch];
       postFixup = ''
         wrapProgram $out/bin/vesktop \
-          --add-flags "--ozone-platform=wayland --enable-zero-copy --use-gl=angle --use-vulkan --use-angle=vulkan --disable-reading-from-canvas --enable-oop-rasterization --enable-raw-draw --enable-gpu-rasterization --enable-gpu-compositing --enable-native-gpu-memory-buffers --enable-accelerated-2d-canvas --enable-accelerated-video-decode --enable-accelerated-mjpeg-decode --disable-gpu-vsync --enable-features=Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport"
-      '';
+          --add-flags "--ozone-platform=wayland --enable-zero-copy --use-gl=angle --use-vulkan --disable-reading-from-canvas --enable-oop-rasterization --enable-raw-draw --enable-gpu-rasterization --enable-gpu-compositing --enable-native-gpu-memory-buffers --enable-accelerated-2d-canvas --enable-accelerated-video-decode --enable-accelerated-mjpeg-decode --disable-gpu-vsync --enable-features=Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport"
+      '';  # Vulkan stuff is left here because we want to make --use-angle=vulkan work.
     }))
   ];
 
